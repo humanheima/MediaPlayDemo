6f5d7bafff80240c62ec9e4d0fe5833fd8dab6c0
dumingwei <dumingwei07050@hellobike.com>
2019-04-28 20:39:46 +0800

    update build.gradle

---
+++ app/src/main/java/com/humanheima/videoplayerdemo/ui/activity/OriginalVideoViewActivity.kt
@@ -0,0 +1,19 @@
+package com.humanheima.videoplayerdemo.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.humanheima.videoplayerdemo.R
+
+/**
+ * Crete by dumingwei on 2019/4/28
+ * Desc: 测试原生的VideoView的使用
+ *
+ */
+class OriginalVideoViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_original_video_view)
+    }
+}
